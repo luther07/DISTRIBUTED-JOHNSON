bb0c4b38c98ef664687daf64cdc8563a6595179c
Mark Johnson <luther07@gmail.com>
2011-02-07 13:32:17 -0600

    add function to re-fill the map

--- hashmod.erl
+++ hashmod.erl
@@ -1,11 +1,11 @@
 -module(hashmod).
--export([hashin/3, hashout/3]).
+-export([hashin/3, hashout/3, rehash/3]).
 
 hashout(TabId, PercentNum, K) ->
-	MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxyz"),
-	RandomMatch = ets:lookup(TabId, MyRandom),
 	case PercentNum>0 of
 		true -> 
+			MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxyz"),
+			RandomMatch = ets:lookup(TabId, MyRandom),
 			case RandomMatch of
 				[] ->
 					hashout(TabId, PercentNum, K);
@@ -21,8 +21,30 @@ hashin(TabId, N, K) ->
 	case N>0 of
 		true ->
 			MyRandom = randomstring:get(K,"abcdefghijklmnopqrstuvwxyz"),
-			ets:insert(TabId,{MyRandom,MyRandom}),
-			N + hashin(TabId, N-1, K);
+			RandomMatch = ets:lookup(TabId, MyRandom),
+			case RandomMatch of
+				[] ->
+					hashin(TabId, N, K);
+				_ ->
+					ets:insert(TabId,{MyRandom,MyRandom}),
+					hashin(TabId, N-1, K)
+			end;
 		false -> N
+	end.
+
+rehash(TabId, PercentNum, K) ->
+	MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxzy"),
+	RandomMatch = ets:lookup(TabId, MyRandom),
+	case PercentNum>0 of
+		true ->
+			case RandomMatch of
+				[] ->
+					rehash(TabId, PercentNum, K);
+				_ ->
+					ets:insert(TabId, {MyRandom, MyRandom}),
+					rehash(TabId, PercentNum-1, K)
+			end;
+		false ->
+			PercentNum
 	end.
 
