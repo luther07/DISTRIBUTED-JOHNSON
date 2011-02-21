0a18fdf39a2c022a3f81fa9fd1269b9f0fa0c742
Mark Johnson <luther07@gmail.com>
2011-02-21 13:40:47 -0600

    some changes

--- Assignment2/RestClient.ml
+++ Assignment2/RestClient.ml
@@ -1,8 +1,10 @@
-let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ;;
-let s_string = "127.0.0.1" ;;
-let s_IAddress = Unix.inet_addr_of_string s_string ;;
-let s_Port = 12345 ;;
-let s_addr = Unix.ADDR_INET(s_IAddress , s_Port) ;;
-Unix.connect s_descr s_addr ;;
-print_endline "Hello Princess!" ;;
-Unix.close s_descr ;;
+let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ;; (* create socket for DATAGRAM *)
+let s_ServerString = "127.0.0.1" ;; (* string for server address *)
+let s_IAddress = Unix.inet_addr_of_string s_string ;; (* internet address for server *)
+let s_Port = 12345 ;; (* port for server *)
+let s_addr = Unix.ADDR_INET(s_IAddress , s_Port) ;; (* ? *)
+Unix.connect s_descr s_addr ;; (* connect to server *)
+(* TODO create datagram *)
+(* TODO write datagram to socket *)
+print_endline "Hello World!" ;; (* test if we get to this line *)
+Unix.close s_descr ;; (* close socket *)
--- Assignment2/RestServer.ml
+++ Assignment2/RestServer.ml
@@ -1,4 +1,4 @@
-let s_descr = Socket.create Unix.PF_INET Unix.SOCK_DGRAM 0 ;;
+let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ;;
 let s_hostname = Unix.gethostname() ;;
 let s_host = Unix.gethostbyname s_hostname ;;
 let s_IAddress = s_host.Unix.h_addr_list.(0) ;;
