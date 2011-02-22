cc94b92c765ba4d3bba81d37c8a15af5fb236c42
Mark Johnson <luther07@gmail.com>
2011-02-21 21:24:29 -0600

    working on client

--- Assignment2/RestClient.ml
+++ Assignment2/RestClient.ml
@@ -1,6 +1,6 @@
 let test_send = 
 	(* let my_bytes = get_bytes_from_file README; *)
-	
+	let my_bytes = String.create 255;
 	let my_address = "127.0.0.1";
 	let my_size = 1024;
 	let my_port = 12345;
@@ -12,12 +12,12 @@ let send_Dgram buff len inet port =
 	let s_IAddress = Unix.inet_addr_of_string s_string ; (* internet address for server *)
 	let s_Port = port ; (* port for server, 12345 *)
 	let s_addr = Unix.ADDR_INET(s_IAddress , s_Port) ; (* ? *)
+	let s_message = "hello_world" ;
 	Unix.connect s_descr s_addr; (* connect to server *)
-	Unix.send s_descr buff 0 1024 [] ;; 
+	Unix.send s_descr s_message 0 (String.length s_message) [] ;
 	(* TODO create datagram *)
 	(* TODO write datagram to socket *)
-	print_endline "Hello World!" ; (* test if we get to this line *)
-	Unix.close s_descr ; (* close socket *)
+	Unix.close s_descr ;; (* close socket *)
 
 (* Copied this function from http://www.codecodex.com/wiki/index.php?title=Read_a_file_into_a_byte_array *)
 let get_bytes_from_file filename =
@@ -26,7 +26,5 @@ let get_bytes_from_file filename =
     (try Buffer.add_channel buf ch max_int with _ -> ());
     close_in ch;
     buf;;
-
-let send_Dgram buff len inet port =
 	 
 
