58cf68f48ff96455ad6ca2443a018cb58e8a5b9b
Mark Johnson <luther07@gmail.com>
2011-02-24 18:48:04 -0600

    listen function is only supported by socket_type SOCK_STREAM and SOCK_SEQPACKET

--- Assignment2/RestServer.ml
+++ Assignment2/RestServer.ml
@@ -1,6 +1,7 @@
-let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ;;
+let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 ;;
 let s_hostname = Unix.gethostname() ;;
 let s_host = Unix.gethostbyname s_hostname ;;
-let s_IAddress = s_host.Unix.h_addr_list.(0) ;;
-let s_address = Unix.ADDR_INET(s_IAddress, 12345) ;;
-Unix.bind s_descr s_address ;;
+let my_addr = s_host.Unix.h_addr_list.(0) ;;
+let s_address = Unix.ADDR_INET(my_addr, 12345) ;;
+Unix.bind s_descr (Unix.ADDR_INET(my_addr, 12345)) ;;
+Unix.listen s_descr 5 ;;
