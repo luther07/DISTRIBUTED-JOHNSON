652409502a979e901f28a1ab832487dda89f4fdd
Mark Johnson <luther07@gmail.com>
2011-02-24 19:03:44 -0600

    we are now listening and waiting for connections

--- Assignment2/RestServer.ml
+++ Assignment2/RestServer.ml
@@ -5,3 +5,4 @@ let my_addr = s_host.Unix.h_addr_list.(0) ;;
 let s_address = Unix.ADDR_INET(my_addr, 12345) ;;
 Unix.bind s_descr (Unix.ADDR_INET(my_addr, 12345)) ;;
 Unix.listen s_descr 5 ;;
+Unix.accept s_descr ;;
