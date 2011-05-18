a74ba5105b9b40afb625e3982ac6a12128fff89f
Mark Johnson <luther07@gmail.com>
2011-05-18 18:22:06 -0500

    change comments

--- Assignment1/tests.erl
+++ Assignment1/tests.erl
@@ -1,14 +1,14 @@
 %%%-----------------------------------------------------------------
 %%% File	: tests.erl
 %%% Author	: Mark Johnson <mjohnson4@luc.edu>
-%%% Purpose	: Tests modules in File "mainprogram.erl"
+%%% Purpose	: Tests modules in module "mainprogram"
 %%% Creeated	: 17 May 2011 by Mark Johnson <mjohnson4@luc.edu>
 %%%-----------------------------------------------------------------
 
 -module(tests).
 -export([test1/3, test2/2]).
 
-%Test function powers in File mainprogram.erl.
+%Test function "powers/2" in module "mainprogram".
 test1(Base, Exp, Res) ->
 	case (mainprogram:powers(Base, Exp)) of
 		Res ->
