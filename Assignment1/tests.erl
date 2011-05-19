d15def66f7a7403394c094ed3d97d6b4c0fe5c48
Mark Johnson <luther07@gmail.com>
2011-05-19 11:29:42 -0500

    making progress on tests

--- Assignment1/tests.erl
+++ Assignment1/tests.erl
@@ -2,14 +2,15 @@
 %%% File	: tests.erl
 %%% Author	: Mark Johnson <mjohnson4@luc.edu>
 %%% Purpose	: Tests modules in module "mainprogram"
-%%% Creeated	: 17 May 2011 by Mark Johnson <mjohnson4@luc.edu>
+%%% Created	: 17 May 2011 by Mark Johnson <mjohnson4@luc.edu>
+%%% Modified	: 19 May 2011 by Mark Johnson <mjohnson4@luc.edu>
 %%%-----------------------------------------------------------------
 
 -module(tests).
--export([test1/3, test2/2]).
+-export([test1/3, test_series/0]).
 
 %Test function "powers/2" in module "mainprogram".
-test1(Base, Exp, Res) ->
+test1(Base, Exp, Result) ->
 	case (mainprogram:powers(Base, Exp)) of
 		Result	->
 				true;
@@ -17,7 +18,26 @@ test1(Base, Exp, Res) ->
 				false
 	end.
 
-%Test return values from powers in File mainprogram.erl.
-test2(Base, Exp) ->
-	mainprogram:powers(Base, Exp).
+%Test series for "powers/2" in module "mainprogram".
+test_series()	->
 	
+	io:fwrite("Test fail: "),
+	io:fwrite(tests:test1(2, 5, 64)),
+
+	io:fwrite("\nTest 0: "),
+	io:fwrite(tests:test1(2, 0, 1)),
+
+	io:fwrite("\nTest 1: "),
+	io:fwrite(tests:test1(2, 1, 2)),
+
+	io:fwrite("\nTest 2: "),
+	io:fwrite(tests:test1(2, 2, 4)),
+
+	io:fwrite("\nTest 3: "),
+	io:fwrite(tests:test1(2, 3, 8)),
+
+	io:fwrite("\nTest 4: "),
+	io:fwrite(tests:test1(2, 4, 16)),
+
+	io:fwrite("\nTest 5: "),
+	io:fwrite(tests:test1(2, 5, 32)).
