c8adf45dc78839dd6c90aa3d55fdb874f9d21cc4
Mark Johnson <luther07@gmail.com>
2011-06-14 20:35:27 -0400

    fixed syntax error; don't know how those got in there

--- Assignment1/mainprogram.erl
+++ Assignment1/mainprogram.erl
@@ -19,72 +19,73 @@
 
 -spec(powers(Base::int(), Expo::int()) -> int()).
 powers(Base, Expo) when Base>0 ->
-			case Expo>0 of
-				true ->
-					Base * powers(Base, Expo-1);
-				false ->
-					1
-			end.
+   case Expo>0 of
+      true ->
+         Base * powers(Base, Expo-1);
+      false ->
+         1
+   end.
 
 %This function gets the plain arguments passed to the erlang init process from the shell script.
 %We are no longer executing this function. We have incorporated this into function "program".
 
--spec(get_args(none()) -> {string(), string(), string()})
+-spec(get_args(none()) -> {string(), string(), string()}).
 get_args() ->
-	MyArgs = init:get_plain_arguments(),
-	case MyArgs of
-		{[]} ->
-			[],
-		{_,_,_} ->
-			Arg1 = string:to_integer(hd(MyArgs)),
-			Arg2 = string:to_integer(hd(tl(MyArgs))),
-			Arg3 = string:to_integer(hd(tl(tl(MyArgs)))),
-			NTemp = element(1, Arg1),
-			N = powers(26, NTemp),
-			D = element(1, Arg2),
-			K = element(1, Arg3),
-			{N, D, K}
-		end.
+   MyArgs = init:get_plain_arguments(),
+      case MyArgs of
+         {_,_,_} ->
+            Arg1 = string:to_integer(hd(MyArgs)),
+            Arg2 = string:to_integer(hd(tl(MyArgs))),
+            Arg3 = string:to_integer(hd(tl(tl(MyArgs)))),
+            NTemp = element(1, Arg1),
+            N = powers(26, NTemp),
+            D = element(1, Arg2),
+            K = element(1, Arg3),
+            {N, D, K}
+      end.
 
 %This is the main function which times all the calls to the methods in module hashmod.
 program() ->	
-	PlainArgs = init:get_plain_arguments(),
-	Arg1 = string:to_integer(hd(PlainArgs)),
-	Arg2 = string:to_integer(hd(tl(PlainArgs))),
-	Arg3 = string:to_integer(hd(tl(tl(PlainArgs)))),
-	NumberOfChars = element(1, Arg1),
-	N = powers(26, NumberOfChars),
-	D = element(1, Arg2),
-	K = element(1, Arg3),
+   PlainArgs = init:get_plain_arguments(),
+   Arg1 = string:to_integer(hd(PlainArgs)),
+   Arg2 = string:to_integer(hd(tl(PlainArgs))),
+   Arg3 = string:to_integer(hd(tl(tl(PlainArgs)))),
+   NumberOfChars = element(1, Arg1),
+   N = powers(26, NumberOfChars),
+   D = element(1, Arg2),
+   K = element(1, Arg3),
 	
-	TabId = ets:new(myTable, [set]),
-	PercentNum = N*D/100,
-	InputTime = timer:tc(hashmod,hashin,[TabId, N, K]),
-	OutputTime = timer:tc(hashmod,hashout,[TabId, PercentNum, K]),
-	ReInputTime = timer:tc(hashmod,rehash,[TabId, PercentNum, K]),
-	NewLine = "\n",
-	Comma = ",",
-	Number = integer_to_list(N),
-	case InputTime of
-		{Time1,_} ->
-			Output1 = integer_to_list(Time1),
-			{ok, F}=file:open("Results.txt", [append]),
-			file:write(F, Number),
-			file:write(F, Comma),
-			file:write(F,Output1),
-			file:write(F,Comma)
-	end,
-	case OutputTime of
-		{Time2,_} ->
-			Output2 = integer_to_list(Time2),
-			file:write(F,Output2),
-			file:write(F,Comma)
-	end,
-	case ReInputTime of
-		{Time3,_} ->
-			Output3 = integer_to_list(Time3),
-			file:write(F,Output3),
-			file:write(F,NewLine),
-			file:close(F)
-	end,
-	init:stop().
+   TabId = ets:new(myTable, [set]),
+   PercentNum = N*D/100,
+   InputTime = timer:tc(hashmod,hashin,[TabId, N, K]),
+   OutputTime = timer:tc(hashmod,hashout,[TabId, PercentNum, K]),
+   ReInputTime = timer:tc(hashmod,rehash,[TabId, PercentNum, K]),
+   NewLine = "\n",
+   Comma = ",",
+   Number = integer_to_list(N),
+   case InputTime of
+      {Time1,_} ->
+         Output1 = integer_to_list(Time1),
+         {ok, F}=file:open("Results.txt", [append]),
+         file:write(F, Number),
+         file:write(F, Comma),
+         file:write(F,Output1),
+         file:write(F,Comma)
+   end,
+   
+   case OutputTime of
+      {Time2,_} ->
+         Output2 = integer_to_list(Time2),
+         file:write(F,Output2),
+         file:write(F,Comma)
+   end,
+	
+   case ReInputTime of
+      {Time3,_} ->
+         Output3 = integer_to_list(Time3),
+         file:write(F,Output3),
+         file:write(F,NewLine),
+         file:close(F)
+   end,
+
+   init:stop().
