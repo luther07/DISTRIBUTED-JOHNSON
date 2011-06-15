68a6b14876431e13ba64a9b358727b1af4f7edfd
Mark Johnson <luther07@gmail.com>
2011-06-14 21:03:01 -0400

    fix line spacing and formatting layout

--- Assignment1/hashmod.erl
+++ Assignment1/hashmod.erl
@@ -9,49 +9,49 @@
 -export([hashin/3, hashout/3, rehash/3]).
 
 hashin(TabId, N, K) -> 
-	case N>0 of
-		true ->
-			MyRandom = randomstring:get(K,"abcdefghijklmnopqrstuvwxyz"),
-			RandomMatch = ets:lookup(TabId, MyRandom),
-			case RandomMatch of
-				[]	->
-					ets:insert(TabId, {MyRandom,MyRandom}),
-					hashin(TabId, N-1, K);
-				_	->
-					hashin(TabId, N, K)
-			end;
-		false -> ok
-	end.
+   case N>0 of
+      true ->
+         MyRandom = randomstring:get(K,"abcdefghijklmnopqrstuvwxyz"),
+         RandomMatch = ets:lookup(TabId, MyRandom),
+         case RandomMatch of
+            [] ->
+               ets:insert(TabId, {MyRandom,MyRandom}),
+               hashin(TabId, N-1, K);
+            _  ->
+               hashin(TabId, N, K)
+         end;
+      false -> ok
+   end.
 
 hashout(TabId, PercentNum, K) ->
-	case PercentNum>0 of
-		true -> 
-			MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxyz"),
-			RandomMatch = ets:lookup(TabId, MyRandom),
-			case RandomMatch of
-				[] ->
-					hashout(TabId, PercentNum, K);
-				_ ->
-					ets:delete(TabId, MyRandom),
-					hashout(TabId, PercentNum-1, K)
-			end;
-		false ->
-			PercentNum
-	end.
+   case PercentNum>0 of
+      true -> 
+         MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxyz"),
+         RandomMatch = ets:lookup(TabId, MyRandom),
+         case RandomMatch of
+            [] ->
+               hashout(TabId, PercentNum, K);
+            _ ->
+               ets:delete(TabId, MyRandom),
+               hashout(TabId, PercentNum-1, K)
+         end;
+      false ->
+         PercentNum
+   end.
 
 rehash(TabId, PercentNum, K) ->
-	case PercentNum>0 of
-		true ->
-			MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxzy"),
-			RandomMatch = ets:lookup(TabId, MyRandom),
-			case RandomMatch of
-				[] ->
-					rehash(TabId, PercentNum, K);
-				_ ->
-					ets:insert(TabId, {MyRandom, MyRandom}),
-					rehash(TabId, PercentNum-1, K)
-			end;
-		false ->
-			PercentNum
-	end.
+   case PercentNum>0 of
+      true ->
+         MyRandom = randomstring:get(K, "abcdefghijklmnopqrstuvwxzy"),
+         RandomMatch = ets:lookup(TabId, MyRandom),
+         case RandomMatch of
+            [] ->
+               rehash(TabId, PercentNum, K);
+            _  ->
+               ets:insert(TabId, {MyRandom, MyRandom}),
+               rehash(TabId, PercentNum-1, K)
+        end;
+      false ->
+         PercentNum
+   end.
 
