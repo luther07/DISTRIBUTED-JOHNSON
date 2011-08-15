c1743207d3ec7ce545259fe3e3dadd081f256241
Mark Johnson <luther07@gmail.com>
2011-08-15 06:59:05 -0500

    I'm working on my UDP communication

--- Assignment2/RestClient.ml
+++ Assignment2/RestClient.ml
@@ -1,30 +1,30 @@
 let test_send = 
-	(* let my_bytes = get_bytes_from_file README; *)
-	let my_bytes = String.create 255;
-	let my_address = "127.0.0.1";
-	let my_size = 1024;
-	let my_port = 12345;
-	send_Dgram my_bytes my_size my_address my_port  ;
+   (* let my_bytes = get_bytes_from_file README; *)
+   let my_bytes = String.create 255;
+   let my_address = "127.0.0.1";
+   let my_size = 1024;
+   let my_port = 12345;
+   send_Dgram my_bytes my_size my_address my_port  ;
 
 let send_Dgram buff len inet port = 
-	let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ; (* create socket for DATAGRAM *)
-	let s_ServerString = inet ; (* string for server address, 127.0.0.1 *)
-	let s_IAddress = Unix.inet_addr_of_string s_string ; (* internet address for server *)
-	let s_Port = port ; (* port for server, 12345 *)
-	let s_addr = Unix.ADDR_INET(s_IAddress , s_Port) ; (* ? *)
-	let s_message = "hello_world" ;
-	Unix.connect s_descr s_addr ; (* connect to server *)
-	Unix.send s_descr s_message 0 (String.length s_message) [] ;
-	(* TODO create datagram *)
-	(* TODO write datagram to socket *)
-	Unix.close s_descr ;; (* close socket *)
+   let s_descriptor = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ; (* create socket for DATAGRAM *)
+   let s_ServerString = inet ; (* string for server address, 127.0.0.1 *)
+   let s_IAddress = Unix.inet_addr_of_string s_string ; (* internet address for server *)
+   let s_Port = port ; (* port for server, 12345 *)
+   let s_addr = Unix.ADDR_INET(s_IAddress , s_Port) ; (* ? *)
+   let s_message = "hello_world" ;
+   (* we do not need to connect to server *)
+   Unix.send s_descr s_message 0 (String.length s_message) [] ;
+   (* TODO create datagram *)
+   (* TODO write datagram to socket *)
+   Unix.close s_descr ;; (* close socket *)
 
 (* Copied this function from http://www.codecodex.com/wiki/index.php?title=Read_a_file_into_a_byte_array *)
 let get_bytes_from_file filename =
-    let ch = open_in filename in
-    let buf = Buffer.create 1024 in
-    (try Buffer.add_channel buf ch max_int with _ -> ());
-    close_in ch;
-    buf;;
+   let ch = open_in filename in
+   let buf = Buffer.create 1024 in
+   (try Buffer.add_channel buf ch max_int with _ -> ());
+   close_in ch;
+   buf;;
 	 
 
--- Assignment2/RestServer.ml
+++ Assignment2/RestServer.ml
@@ -1,6 +1,12 @@
-let s_descr = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 17 ;;
+#load "unix.cma";
+let s_descriptor = Unix.socket Unix.PF_INET Unix.SOCK_DGRAM 0 ;;
 let s_hostname = Unix.gethostname() ;;
 let s_host = Unix.gethostbyname s_hostname ;;
 let my_addr = s_host.Unix.h_addr_list.(0) ;;
 let s_address = Unix.ADDR_INET(my_addr, 12345) ;;
-Unix.bind s_descr (Unix.ADDR_INET(my_addr, 12345)) ;;
+Unix.bind s_descriptor (Unix.ADDR_INET(my_addr, 12345)) ;;
+(* recvfrom or recvmsg... *)
+(*let buffer = String.create max_to_read in
+   while true do
+      ignore (Unix.recvfrom s_descriptor, buffer, 0, max_to_read []);
+   done*)
